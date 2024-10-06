ba69f0a6ec6dc090f220a173f186703f7811cf1d
Endre Vegh <github.com.guiding230@passmail.net>
2024-10-06 18:49:34 +0100

    experiment: logs

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export const dynamic = 'force-dynamic'
 export default async function Home() {
   const res = await fetch('https://rickandmortyapi.com/api/character/54')
 
+  console.log('from rsc')
+
   const json = await res.json()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
