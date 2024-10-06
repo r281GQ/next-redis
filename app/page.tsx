48e326f0e2b5dceb6c09a624bd6c4d8d57a60c4b
Endre Vegh <github.com.guiding230@passmail.net>
2024-10-06 11:32:08 +0100

    experiment: force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
-export default function Home() {
+export const dynamic = 'force-dynamic'
+// 'auto' | 'force-dynamic' | 'error' | 'force-static'
+
+export default async function Home() {
+  const res = await fetch('https://rickandmortyapi.com/api/character/54')
+
+  const json = await res.json()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {JSON.stringify(json)}
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
